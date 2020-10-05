a61604bd143842333470d4230961e1bc6e619ae6
xYanchi <haymondyantech@gmail.com>
2020-10-05 01:12:23 -0400

    Added backend Link

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     //The API (cloud function) URL
-    baseURL: 'http://localhost:5001/clone-7310d/us-central1/api'
+    baseURL: 'https://us-central1-clone-7310d.cloudfunctions.net/api' //Live url from Cloud functions (firebase)
+    // 'http://localhost:5001/clone-7310d/us-central1/api'
 });
 
 export default instance;
